6c9f8ecc426dd28a48bc2f8b2db13cdfaa0fa892
sugu2100 <sugu2100@daum.net>
2021-12-09 11:52:05 +0900

    session 권한부여

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ def getconn():
 
 @app.route('/')  #127.0.0.1:5000
 def index():
-    return render_template('index.html')
-    #return "<h1>Welcome~ 방문을 환영합니다.</h1>"
+    if 'userID' in session:   #session에 userID가 존재하면
+        ssid = session.get('userID')  # 세션을 가져옴
+        return render_template('index.html', ssid=ssid) #index로 보내기
+    else:
+        return render_template('index.html')
 
 @app.route('/memberlist/')
 def memberlist():
@@ -21,7 +24,11 @@ def memberlist():
     cur.execute(sql)
     rs = cur.fetchall()   #db에서 검색한 데이터
     conn.close()
-    return render_template('memberlist.html', rs=rs)
+    if 'userID' in session:  # session에 userID가 존재하면
+        ssid = session.get('userID')  # 세션을 가져옴
+        return render_template('memberlist.html', ssid=ssid, rs=rs)
+    else:
+        return render_template('memberlist.html')
 
 @app.route('/member_view/<string:id>/')
 def member_view(id):  #mid를 경로로 설정하고 매개변수로 넘겨줌
@@ -31,7 +38,11 @@ def member_view(id):  #mid를 경로로 설정하고 매개변수로 넘겨줌
     cur.execute(sql)
     rs = cur.fetchone()  #해당 1개의 자료를 반환받음
     conn.close()
-    return render_template('member_view.html', rs=rs)
+    if 'userID' in session:  # session에 userID가 존재하면
+        ssid = session.get('userID')  # 세션을 가져옴
+        return render_template('member_view.html', rs=rs, ssid=ssid)
+    else:
+        return render_template('member_view.html')
 
 @app.route('/register/', methods = ['GET', 'POST'])  #url 경로
 def register():
@@ -80,5 +91,47 @@ def login():
 def logout():
     session.pop('userID')  #세션 삭제
     return redirect(url_for('index'))
+
+@app.route('/member_del/<string:id>/')   #삭제 url 생성
+def member_del(id):  #mid를 매개변수로 넘겨줌
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "DELETE FROM member WHERE mid = '%s' " % (id)
+    cur.execute(sql)  # 삭제 수행
+    conn.commit()     # 수행 완료
+    conn.close()
+    return redirect(url_for('memberlist'))
+
+@app.route('/member_edit/<string:id>/', methods = ['GET', 'POST'])
+def member_edit(id):
+    if request.method == "POST":
+        #자료 넘겨 받음
+        id = request.form['mid']
+        pwd = request.form['passwd']
+        name = request.form['name']
+        age = request.form['age']
+        date = request.form['regDate']
+        #db 연결
+        conn = getconn()
+        cur = conn.cursor()
+        sql = "UPDATE member SET passwd = '%s', name = '%s' , age = '%s', regDate = '%s' " \
+              "WHERE mid = '%s' " % (pwd, name, age, date, id)
+        cur.execute(sql)  # 실행 함수
+        conn.commit()  # 커밋 완료
+        conn.close()
+        return redirect(url_for('member_view', id=id))  # member_view 해당 id로 이동
+    else:
+        # 회원 자료 가져오기
+        conn = getconn()
+        cur = conn.cursor()
+        sql = "SELECT * FROM member WHERE mid = '%s' " % (id)
+        cur.execute(sql)
+        rs = cur.fetchone()
+        conn.close()
+        if 'userID' in session:
+            ssid = session.get('userID')
+            return render_template('member_edit.html', rs=rs, ssid=ssid)
+        else:
+            return render_template('member_edit.html')
 
 app.run(debug=True)
